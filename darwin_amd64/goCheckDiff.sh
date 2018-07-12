62f52c1cdb7c68cf09c951bf8df2390f60c17b9e
koreeda <koreeda@groovy-m.com>
2018-07-12 12:34:35 +0900

    modified sample shell

--- darwin_amd64/goCheckDiff.sh
+++ darwin_amd64/goCheckDiff.sh
@@ -2,9 +2,7 @@
 
 cd `dirname $0`
 
-# baseDir="/Users/NaokiKoreeda/dir1/"
-# targetDir="/Users/NaokiKoreeda/dir2/"
-baseDir="/Users/gurubimakku/work/dir1/"
-targetDir="/Users/gurubimakku/work/dir2/"
+baseDir="/Users/NaokiKoreeda/dir1/"
+targetDir="/Users/NaokiKoreeda/dir2/"
 
 ./goCheckDiff ${baseDir} ${targetDir}
